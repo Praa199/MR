bb51678417555a7cb922296ef871b29ceb9e6f34
Aldo Peña <aldo.pena.93@hotmail.com>
2021-01-26 15:12:04 +0100

    added a few changes, splash screen and game over activate and scores update

--- source/index.js
+++ source/index.js
@@ -3,10 +3,10 @@ let ctx = canvas.getContext('2d')
 let splash = document.querySelector('.splash')
 let gOver = document.querySelector('.game-over')
 
-let lives = []
 
 let intervalId = 0 
 let resource = 0;
+let lives = 3
 
 let moonImg = document.createElement('img')
 moonImg.src = '/images/moon NASA1.jpg'
@@ -36,8 +36,9 @@ function draw(){
     
     drawCraters()
     drawIces()
-    drawScore()
     drawRover()
+    drawScore()
+    drawLives()
 }
 
 
@@ -48,19 +49,19 @@ function drawRover(){
             downArrow = false;
             
             if (roverXY.y >= 130) {
-            roverXY.y -= 1
-            
-        } 
-    }
-    else if (event.keyCode == 40 || event.key == "ArrowDown") {
-        upArrow = false;
-        downArrow = true;
-        if (roverXY.y <= 550) {
-            roverXY.y += 1
-            
+                roverXY.y -= 1
+                
+            } 
         }
-    }
-    console.log(roverXY.y)
+        else if (event.keyCode == 40 || event.key == "ArrowDown") {
+            upArrow = false;
+            downArrow = true;
+            if (roverXY.y <= 550) {
+                roverXY.y += 1
+                
+            }
+        }
+
 })
 
 document.addEventListener('keyup', (event) => {
@@ -72,38 +73,45 @@ ctx.drawImage(rover, 758, roverXY.y)
 ctx.drawImage(earth, 0, 5)
 }
 
+
 function drawCraters(){
     ctx.drawImage(moonImg, 0, 0)
     
     for (let i = 0; i < craters.length; i++) {
     ctx.drawImage(singleCrater, craters[i].x, craters[i].y )
     craters[i].x+=5
-    craters[i].y+=.1
+    craters[i].y+=.5
     
     if (craters[i].x == 300 ) {
-        craters.push(
-            {
-                x: -50, 
-                y: Math.floor(Math.random() * canvas.height -135)
-            }
-            )
+        craters.push( {
+
+            x: -50, 
+            y: Math.floor(Math.random() * canvas.height -135)
         }
+        )
+    }
+    
+    if (craters[i].x + (singleCrater.width -50) >= roverXY.x &&
+        (roverXY.y > craters[i].y &&  
+            (rover.y + rover.height < craters[i].y + (singleCrater.height - 20)))) {
+            console.log('ouch')
+        craters[i].x = 2000 
+        craters[i].y =  2000 
+        lives--
     }
 
-    if (craters.x + singleCrater.width >= roverXY.x + rover.width && craters.y + singleCrater.height >= roverXY.y + rover.height ) {
-        console.log('ouch')
-        lives.push('ouch')
-        if(lives.length == 1){
-            return gameOver()
-        }
+    if(lives == 0){
+        gameOver()
     }
 }
+}
+
 
 function drawIces(){
     for (let j = 0; j < ices.length; j++) {
         ctx.drawImage(singleIce, ices[j].x, ices[j].y )
     ices[j].x+=5
-    ices[j].y+=.1
+    ices[j].y+=.5
     
     if (ices[j].x == 400) {
         ices.push(
@@ -112,21 +120,35 @@ function drawIces(){
                 y: Math.floor(Math.random() * canvas.height - 50) 
             }
             )
-    
-    }
-}}
+            
+        }
 
-function drawScore() {
+        if (ices[j].x + (singleIce.width -10) >= roverXY.x &&
+        (roverXY.y > ices[j].y &&  
+            (rover.y + rover.height < ices[j].y + (singleCrater.height - 10)))) {
+                console.log('yay')
+                ices[j].x = 2000 
+                ices[j].y =  2000 
+                resource++
+            }
+        }
+    }
+
+    
+    function drawScore() {
 ctx.drawImage(singleIce, 585, 10 )
 ctx.font = '30px Verdana '
 ctx.fillStyle = 'white'
 ctx.fillText('Resource:  ' + resource, 650, 50)
 }    
-/* intervalId = setInterval(() => {
-    requestAnimationFrame(draw)
-    console.log('hey')
-}, 10000)
- */
+
+
+function drawLives(){
+ctx.font = '30px Verdana '
+ctx.fillStyle = 'white'
+ctx.fillText('Lives: ' + lives, 700, 90)
+}
+
 
 function startGame(){
     gOver.style.display = 'none'
@@ -154,8 +176,15 @@ window.addEventListener('load', () => {
 
 
 function gameOver() {
-    canvas.remove()
+    canvas.style.display = 'none'
     gOver.style.display = 'block'
+    
+    let replayBtn = document.querySelector('#replay')
+    
+    replayBtn.addEventListener('click', () => {
+        startGame()    
+        clearInterval(intervalId)
+    })
 }
 
 
